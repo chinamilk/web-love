7396d06da307f6c4bf70a07c8c42bb08b83f344f
小光 <30540766+xg-gmg@users.noreply.github.com>
2018-11-04 21:51:06 +0800

    使用前需更改数据库配置
    使用前需更改数据库配置

--- config.php
+++ config.php
@@ -6,6 +6,6 @@
  * Time: 14:23
  */
 //数据库相关配置，使用前需配置好
-define('MYSQL_HOST','58.87.121.14');//数据库地址
+define('MYSQL_HOST','localhost');//数据库地址
 define('MYSQL_USER','weblove');//数据库用户名
-define('MYSQL_PW','bxjcg..123..');//数据库密码
+define('MYSQL_PW','123456789');//数据库密码
