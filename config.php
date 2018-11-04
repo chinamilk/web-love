f78504a0e6636a223bb17546b2854d891feebe5a
小光 <30540766+xg-gmg@users.noreply.github.com>
2018-11-04 21:50:06 +0800

    数据库配置文件
    使用前需配置好这个文件

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xg
+ * Date: 2018/11/4
+ * Time: 14:23
+ */
+//数据库相关配置，使用前需配置好
+define('MYSQL_HOST','58.87.121.14');//数据库地址
+define('MYSQL_USER','weblove');//数据库用户名
+define('MYSQL_PW','bxjcg..123..');//数据库密码
