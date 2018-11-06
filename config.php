d1bf8db334c13cef95def61df4b3c4fd743919a5
小光 <30540766+xg-gmg@users.noreply.github.com>
2018-11-06 08:14:03 +0800

    删除多余代码
    删除掉多余的注释信息

--- config.php
+++ config.php
@@ -1,11 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: xg
- * Date: 2018/11/4
- * Time: 14:23
- */
 //数据库相关配置，使用前需配置好
 define('MYSQL_HOST','localhost');//数据库地址
 define('MYSQL_USER','weblove');//数据库用户名
 define('MYSQL_PW','123456789');//数据库密码
+?>
